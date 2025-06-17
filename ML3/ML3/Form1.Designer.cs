299152a3036adb8dd8964b94c4dfca6be26771e4
Dziabenko Oleksandr <velit79845@gmail.com>
2025-06-17 18:17:29 +0300

    Movie Library

---
+++ ML3/ML3/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace MovieDatabase
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.Label labelYear;
+        private System.Windows.Forms.Button btnSearch;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.labelYear = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeight = 29;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 63);
+            this.dataGridView1.MaximumSize = new System.Drawing.Size(560, 241);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(560, 241);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+
+            this.txtYear.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.txtYear.Font = new System.Drawing.Font("Rockwell", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtYear.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtYear.Location = new System.Drawing.Point(179, 15);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(120, 39);
+            this.txtYear.TabIndex = 1;
+
+            this.labelYear.AutoSize = true;
+            this.labelYear.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.labelYear.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelYear.Location = new System.Drawing.Point(12, 15);
+            this.labelYear.Name = "labelYear";
+            this.labelYear.Size = new System.Drawing.Size(173, 24);
+            this.labelYear.TabIndex = 2;
+            this.labelYear.Text = "Введіть рік фільму:";
+
+            this.btnSearch.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSearch.Font = new System.Drawing.Font("Script MT Bold", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Image = global::ML3.Properties.Resources.Screenshot_2025_06_16_170120;
+            this.btnSearch.Location = new System.Drawing.Point(305, 15);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(267, 45);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Пошук";
+            this.btnSearch.UseVisualStyleBackColor = false;
+
+            this.BackgroundImage = global::ML3.Properties.Resources.Screenshot_2025_06_11_185948;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(584, 341);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.labelYear);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.dataGridView1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Фільми Темної Сторони";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ ML3/ML3/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Diagnostics;
+using WMPLib;
+
+namespace MovieDatabase
+{
+    public partial class Form1 : Form
+    {
+        private WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+
+        List<Movie> allMovies = new List<Movie>
+        {
+            new Movie { Title = "The Phantom Menace", Year = 1999, Url = "https://tv.apple.com/ua/movie/star-wars-the-phantom-menace/umc.cmc.1x8mput5lh4l0u6vue0hbmhs3" },
+            new Movie { Title = "Attack of the Clones", Year = 2002, Url = "https://tv.apple.com/ua/movie/star-wars-attack-of-the-clones/umc.cmc.27tj6w4r4fpeziazh0cyza2bq" },
+            new Movie { Title = "Revenge of the Sith", Year = 2005, Url = "https://tv.apple.com/ua/movie/star-wars-episode-iii--revenge-of-the-sith/umc.cmc.5nolvqgurfe17eln52s0b7j6j?action=play" },
+            new Movie { Title = "A New Hope", Year = 1977, Url = "https://tv.apple.com/ua/movie/star-wars-episode-iv---a-new-hope/umc.cmc.2o65qvudvwq1l1rqjlbyfszwn" },
+            new Movie { Title = "The Empire Strikes Back", Year = 1980, Url = "https://tv.apple.com/ua/movie/star-wars-the-empire-strikes-back/umc.cmc.3vabzkcmuoxxm3r3ng9q4rgh9" },
+            new Movie { Title = "Return of the Jedi", Year = 1983, Url = "https://tv.apple.com/ua/movie/star-wars-episode-vi---return-of-the-jedi/umc.cmc.3ggalukb7xb5pf1gfgdlwkcx" },
+            new Movie { Title = "The Clone Wars", Year = 2008, Url = "https://www.lostfilm.tv/series/Star_Wars_The_Clone_Wars/seasons" },
+            new Movie { Title = "Rogue One", Year = 2016, Url = "https://tv.apple.com/ua/movie/rogue-one-a-star-wars-story/umc.cmc.5ydra856u0y2hc1ur13qd4xxx?action=play" },
+            new Movie { Title = "Solo", Year = 2018, Url = "https://tv.apple.com/ua/movie/solo-a-star-wars-story/umc.cmc.1skhjjrps93jxwc8w45mc4yyj" },
+            new Movie { Title = "The Mandalorian", Year = 2019, Url = "https://www.lostfilm.tv/series/The_Mandalorian" }
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            SetupDataGridView();
+
+            WindowsMediaPlayer player = new WindowsMediaPlayer();
+
+            ShowMovies(allMovies);
+
+            btnSearch.Click += BtnSearch_Click;
+            dataGridView1.CellContentClick += DataGridView1_CellContentClick;
+        }
+
+        private void SetupDataGridView()
+        {
+            dataGridView1.Columns.Clear();
+
+
+            DataGridViewTextBoxColumn colTitle = new DataGridViewTextBoxColumn();
+            colTitle.Name = "Title";
+            colTitle.HeaderText = "Назва";
+            colTitle.DataPropertyName = "Title";
+            colTitle.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns.Add(colTitle);
+
+
+            DataGridViewTextBoxColumn colYear = new DataGridViewTextBoxColumn();
+            colYear.Name = "Year";
+            colYear.HeaderText = "Рік";
+            colYear.DataPropertyName = "Year";
+            colYear.Width = 60;
+            dataGridView1.Columns.Add(colYear);
+
+
+            DataGridViewButtonColumn colWatch = new DataGridViewButtonColumn();
+            colWatch.Name = "Watch";
+            colWatch.HeaderText = "";
+            colWatch.Text = "Дивитися онлайн";
+            colWatch.UseColumnTextForButtonValue = true;
+            colWatch.Width = 120;
+            dataGridView1.Columns.Add(colWatch);
+
+            dataGridView1.AutoGenerateColumns = false;
+        }
+
+        private void ShowMovies(List<Movie> movies)
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = movies;
+        }
+
+        private void BtnSearch_Click(object sender, EventArgs e)
+        {
+            string input = txtYear.Text.Trim();
+            if (string.Equals(input, "Order 66", StringComparison.OrdinalIgnoreCase))
+            {
+                try
+                {
+                    player.URL = "order66.mp3";
+                    player.controls.play();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Не вдалося відтворити звук: " + ex.Message);
+                }
+                return;
+            }
+
+            if (string.IsNullOrEmpty(input))
+            {
+                ShowMovies(allMovies);
+                return;
+            }
+
+            if (int.TryParse(input, out int year))
+            {
+                var filtered = allMovies.Where(m => m.Year == year).ToList();
+                ShowMovies(filtered);
+            }
+            else
+            {
+                MessageBox.Show("Введіть коректний рік.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+
+if (dataGridView1.Columns[e.ColumnIndex].Name == "Watch")
+            {
+                Movie movie = (Movie)dataGridView1.Rows[e.RowIndex].DataBoundItem;
+                if (!string.IsNullOrEmpty(movie.Url))
+                {
+                    try
+                    {
+                        Process.Start(new ProcessStartInfo
+                        {
+                            FileName = movie.Url,
+                            UseShellExecute = true
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Не вдалося відкрити посилання: " + ex.Message);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Посилання на фільм відсутнє.");
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+
+    public class Movie
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Url { get; set; }
+    }
+}
